3e231877731ce70ff0e2af635162c7c7c801142d
Dennis Chicaiza A <dchicaiza@webcreek.com>
2023-06-01 19:14:08 +0200

    Feat: First console example of Factory Method pattern

---
+++ DesignPattern/FactoryPattern/Factory.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace DesignPattern.FactoryPattern
+{
+	//CREATOR - ABSTRACT CLASS
+	public abstract class SaleFactory
+	{
+		public abstract ISale GetSale();
+	}
+
+    //CONCRETE CREATOR - Factory
+    public class StoreSaleFactory: SaleFactory
+    {
+
+        private decimal _extra;
+
+        public StoreSaleFactory(decimal extra)
+        {
+            _extra = extra;
+        }
+ 
+        public override ISale GetSale()
+        {
+            return new StoreSale(_extra);
+        }
+    }
+
+    //CONCRETE CREATOR - Factory - Internet Sale Factory
+    public class InternetSaleFactory : SaleFactory
+    {
+
+        private decimal _discount;
+
+        public InternetSaleFactory(decimal discount)
+        {
+            _discount = discount;
+        }
+
+        public override ISale GetSale()
+        {
+            return new StoreSale(_discount);
+        }
+        
+    }
+
+    //Concret Product
+    public class StoreSale : ISale
+    {
+        private decimal _extra;
+
+        public StoreSale(decimal extra)
+        {
+            _extra = extra;
+        }
+
+        public void Sell(decimal total)
+        {
+            Console.WriteLine($"La venta tiene un total de {total + _extra}");
+        }
+    }
+
+    //Concret Product
+    public class InternetSale : ISale
+    {
+        private decimal _discount;
+
+        public InternetSale(decimal discount)
+        {
+            _discount = discount;
+        }
+
+        public void Sell(decimal total)
+        {
+            Console.WriteLine($"La venta tiene un total de {total - _discount}");
+        }
+    }
+
+    //Product
+    public interface ISale
+	{
+		public void Sell(decimal total);
+	}
+}
+
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesignPattern.FactoryPattern;
 using DesignPattern.Singleton;
 
+
+//SINGLETON PATTERN
 Console.WriteLine("Hello, World!");
 
 var singleton = Singleton.Instance;
@@ -14,3 +17,14 @@ var a = Singleton.Instance;
 var b = Singleton.Instance;
 
 Console.WriteLine(a == b);
+
+//FACTORY METHOD PATTERN
+//adding 10 extra bucks
+SaleFactory storeSaleFactory = new StoreSaleFactory(10);
+//Discounting 15$
+SaleFactory internetSaleFactory = new InternetSaleFactory(15);
+
+ISale sale1 = storeSaleFactory.GetSale();
+sale1.Sell(100);
+ISale sale2 = internetSaleFactory.GetSale();
+sale2.Sell(120);
